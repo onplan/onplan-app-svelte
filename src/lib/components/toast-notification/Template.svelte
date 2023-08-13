71d3c9aa072f0d620b27db01dd7bef6ea2de24ff
Jose Clet II <clet.jose@gmail.com>
2023-08-13 09:48:04 +0800

    Change toast transition to fly

--- src/lib/components/toast-notification/Template.svelte
+++ src/lib/components/toast-notification/Template.svelte
@@ -1,5 +1,6 @@
 <script>
-	import { fade } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+	import { fly } from 'svelte/transition';
 
 	// Props passed by `svelte-notifications'
 	export let notification = {};
@@ -48,7 +49,13 @@
 
   Based on Bootstrap 5 toast : https://getbootstrap.com/docs/5.2/components/toasts/
 -->
-<div class="toast show m-1" role="alert" aria-live="assertive" aria-atomic="true" transition:fade>
+<div
+	class="toast show m-1"
+	role="alert"
+	aria-live="assertive"
+	aria-atomic="true"
+	transition:fly={{ duration: 300, x: 100, easing: quintOut }}
+>
 	<div class="toast-header">
 		<!-- <img src="..." class="rounded me-2" alt="..." /> -->
 		<i class={`bi ${bIcon} ${textColor}`} />
