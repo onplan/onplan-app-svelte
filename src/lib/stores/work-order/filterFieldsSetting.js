df4769556e90a056c31792fb9ab94a1d10dc63ec
Jose Clet II <clet.jose@gmail.com>
2023-08-21 18:08:42 +0800

    Refactor `filterFieldsSetting` store

--- src/lib/stores/work-order/filterFieldsSetting.js
+++ src/lib/stores/work-order/filterFieldsSetting.js
@@ -8,12 +8,20 @@ import { DEFAULT_FILTER_FIELDS_SETTING } from '$lib/utils/constants';
 
 const FILTER_FIELDS_SETTING_KEY = 'filterFieldsSetting';
 
-let persistedSetting = localStorage.getItem(FILTER_FIELDS_SETTING_KEY);
-
-// create a `writtable store`
-const filterFieldsSetting = writable(
-	persistedSetting ? JSON.parse(persistedSetting) : DEFAULT_FILTER_FIELDS_SETTING
-);
+/**
+ * Filter Fields Setting.
+ *
+ * Object that contains Switches states for `myJobsFilter`, `assetFilter`, `statusFilter`, `scheduledFromDate` and `scheduledToDate`
+ *
+ * Writtable store
+ */
+const filterFieldsSetting = writable(DEFAULT_FILTER_FIELDS_SETTING, (set) => {
+	// Initialization
+	let persistedSetting = localStorage.getItem(FILTER_FIELDS_SETTING_KEY);
+	if (persistedSetting) {
+		set(JSON.parse(persistedSetting));
+	}
+});
 
 // Update localstorage once updated (User applied new filters)
 filterFieldsSetting.subscribe((newSettings) => {
