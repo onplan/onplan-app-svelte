215436ce4fadff0af1043814e3f023f3363de210
Jose Clet II <clet.jose@gmail.com>
2023-08-28 11:24:40 +0800

    Add WO list bottom info

--- src/lib/components/routes/index/WorkOrdersLists.svelte
+++ src/lib/components/routes/index/WorkOrdersLists.svelte
@@ -7,6 +7,26 @@
 
 	import WorkOrderStatus from '$lib/components/render/WorkOrderStatus.svelte';
 	import WorkOrderClass from '$lib/utils/class/WorkOrderClass';
+	import filterFieldsSetting from '$lib/stores/work-order/filterFieldsSetting';
+	import { isWorkingOffline } from '$lib/stores/connectivity';
+	import { WORK_ORDER_STATUS } from '$lib/utils/constants';
+
+	let filteredWOsInfo = '';
+	// Update `filteredWOsInfo` when `$filterFieldsSetting` store changes
+	$: {
+		let infoMessage = '';
+
+		if (!$isWorkingOffline) {
+			const ffs = $filterFieldsSetting;
+
+			infoMessage += ffs.statusFilter?.map((status) => WORK_ORDER_STATUS[status]).join(' / ');
+			infoMessage += ffs.myJobsFilter ? ' jobs' : ' from other crews';
+		} else {
+			infoMessage = 'saved on device';
+		}
+
+		filteredWOsInfo = `Showing ${$WOLists.length} ${infoMessage}`;
+	}
 </script>
 
 <!--
@@ -16,84 +36,100 @@
 
   Contains all viewable WOs based on the filter settings
 -->
-<ListGroup flush class="mt-md-1" style="min-height: 100vh !important;">
-	<!-- CONTINUE HERE ....... -->
-	{#each $WOLists as wo (wo.id)}
-		{@const woData = new WorkOrderClass(wo)}
-		{@const {
-			totaStepsCount,
-			p1DefectsCount,
-			p2DefectsCount,
-			p3DefectsCount,
-			completedStepsCount
-		} = woData.getStepsCountDetails()}
+<div style="min-height: 100vh !important;">
+	<ListGroup flush class="mt-md-1">
+		{#each $WOLists as wo (wo.id)}
+			{@const woData = new WorkOrderClass(wo)}
+			{@const {
+				totaStepsCount,
+				p1DefectsCount,
+				p2DefectsCount,
+				p3DefectsCount,
+				completedStepsCount
+			} = woData.getStepsCountDetails()}
 
-		<ListGroupItem>
-			<div class="work-order mt-2">
-				<div class="clearfix" style="height: 40px;">
-					<WorkOrderStatus
-						status={wo.status}
-						completionMode={wo?.customer?.isWorkOrderFinalisationRequired}
-					/>
+			<ListGroupItem>
+				<div class="work-order mt-2">
+					<div class="clearfix" style="height: 40px;">
+						<WorkOrderStatus
+							status={wo.status}
+							completionMode={wo?.customer?.isWorkOrderFinalisationRequired}
+						/>
 
-					<p class="float-end">
-						<span class="wo-date">
-							<i class="bi bi-clock" />
-							<small> {asDate(woData.getDisplayDate())} </small>
-						</span>
-						<PieProgress percentage={wo.progress.percent} statusColor={woData.getStatusColor()} />
-					</p>
-				</div>
-
-				<p class="wo-title my-0">{wo.workOrderNumber}</p>
-
-				<div>{woData.getTaskDetails()}</div>
-
-				<div class="mt-1">
-					<i class="bi bi-gear-fill text-muted" />
-					<strong>{wo.customerAssetList.assetFloc}</strong>
-					- {wo.customerAssetList.assetName}
-				</div>
-
-				<div class="clearfix mt-1">
-					<div class="badge bg-secondary bg-opacity-25 text-dark rounded-1 mt-1">
-						{wo.modelNumber}
+						<p class="float-end">
+							<span class="wo-date">
+								<i class="bi bi-clock" />
+								<small> {asDate(woData.getDisplayDate())} </small>
+							</span>
+							<PieProgress percentage={wo.progress.percent} statusColor={woData.getStatusColor()} />
+						</p>
 					</div>
-					<div class="float-end mt-1">
-						<small class=""> {wo.customer.customerSite} </small>
+
+					<p class="wo-title my-0">{wo.workOrderNumber}</p>
+
+					<div>{woData.getTaskDetails()}</div>
+
+					<div class="mt-1">
+						<i class="bi bi-gear-fill text-muted" />
+						<strong>{wo.customerAssetList.assetFloc}</strong>
+						- {wo.customerAssetList.assetName}
+					</div>
+
+					<div class="clearfix mt-1">
+						<div class="badge bg-secondary bg-opacity-25 text-dark rounded-1 mt-1">
+							{wo.modelNumber}
+						</div>
+						<div class="float-end mt-1">
+							<small class=""> {wo.customer.customerSite} </small>
+						</div>
+					</div>
+
+					<div class="clearfix mt-1">
+						<div class="float-start">
+							<i class="bi bi-check-square-fill text-success text-opacity-75" />
+							<small class="ms-1">{completedStepsCount}/{totaStepsCount}</small>
+
+							<span class="ms-2 badge rounded-pill bg-danger p1to3">P1</span>
+							<small class="ms-1">{p1DefectsCount}</small>
+
+							<span class="ms-2 badge rounded-pill bg-warning p1to3">P2</span>
+							<small class="ms-1">{p2DefectsCount}</small>
+
+							<span class="ms-2 badge rounded-pill bg-secondary p1to3">P3</span>
+							<small class="ms-1">{p3DefectsCount}</small>
+						</div>
+						<div class="float-end">
+							<!--  `wo.saved` wont exist if saved, or will with saved = 1 temporary ('saved' does not get returned from server) -->
+							{#if wo.saved !== null && Number(wo.saved) === 0}
+								<!-- WO Not Synced -->
+								<i class="bi bi-cloud-slash text-danger" />
+							{:else}
+								<!-- WO Synced to server-->
+								<i class="bi bi-cloud-check-fill text-success text-opacity-75" />
+							{/if}
+						</div>
 					</div>
 				</div>
+			</ListGroupItem>
+		{:else}
+			<p class="text-center mt-3">No matching records found</p>
+		{/each}
+	</ListGroup>
 
-				<div class="clearfix mt-1">
-					<div class="float-start">
-						<i class="bi bi-check-square-fill text-success text-opacity-75" />
-						<small class="ms-1">{completedStepsCount}/{totaStepsCount}</small>
+	<div class="text-center border-top mt-2 py-4 px-3">
+		<p>{filteredWOsInfo}</p>
 
-						<span class="ms-2 badge rounded-pill bg-danger p1to3">P1</span>
-						<small class="ms-1">{p1DefectsCount}</small>
-
-						<span class="ms-2 badge rounded-pill bg-warning p1to3">P2</span>
-						<small class="ms-1">{p2DefectsCount}</small>
-
-						<span class="ms-2 badge rounded-pill bg-secondary p1to3">P3</span>
-						<small class="ms-1">{p3DefectsCount}</small>
-					</div>
-					<div class="float-end">
-						<!--  `wo.saved` wont exist if saved, or will with saved = 1 temporary ('saved' does not get returned from server) -->
-						{#if wo.saved !== null && Number(wo.saved) === 0}
-							<!-- WO Not Synced -->
-							<i class="bi bi-cloud-slash text-danger" />
-						{:else}
-							<!-- WO Synced to server-->
-							<i class="bi bi-cloud-check-fill text-success text-opacity-75" />
-						{/if}
-					</div>
-				</div>
-			</div>
-		</ListGroupItem>
-		<!-- content here -->
-	{/each}
-</ListGroup>
+		<!-- Todo: Make this `Load More` functional -->
+		<button
+			type="button"
+			class="btn btn-primary btn-sm mx-auto px-3"
+			style="--bs-btn-border-radius: 1rem;"
+			disabled={$isWorkingOffline}
+		>
+			{$isWorkingOffline ? 'Go online to load more work orders' : 'Load More'}
+		</button>
+	</div>
+</div>
 
 <style>
 	.wo-date {
